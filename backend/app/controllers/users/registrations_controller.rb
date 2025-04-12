194cd7ef1c697eea3206e9458837a0f5622ede59
ogami000 <k-ogami@lainz.jp>
2025-04-12 14:25:52 +0900

    devise second

---
+++ backend/app/controllers/users/registrations_controller.rb
@@ -0,0 +1,25 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  respond_to :json
+
+  private
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: '登録が完了しました。' },
+        data: resource
+      }
+    else
+      render json: {
+        status: { code: 422, message: "登録に失敗しました。" },
+        errors: resource.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
+end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable,
-         :registerable,
-         :jwt_authenticatable,
-         jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null,
-         :validatable
+        :registerable,
+        :validatable,
+        :jwt_authenticatable,
+        jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 end
